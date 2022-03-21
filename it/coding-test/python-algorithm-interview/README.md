ffd4db1e996b27cb208c9f11616e6863833c2863
khh <khh@omnicns.com>
2022-03-21 15:58:53 +0900

    test , readme

--- it/coding-test/python-algorithm-interview/README.md
+++ it/coding-test/python-algorithm-interview/README.md
@@ -1,6 +1,9 @@
 tip
 ===
-점프 투 파이썬
+# 파이썬 알고리즘 인터뷰
+- https://github.com/onlybooks/algorithm-interview
+
+# 점프 투 파이썬
  - https://wikidocs.net/book/1
  - for: https://wikidocs.net/22
  - regex: https://docs.python.org/3/library/re.html
--- it/coding-test/python-algorithm-interview/test/test.py
+++ it/coding-test/python-algorithm-interview/test/test.py
@@ -1,3 +1,11 @@
-import pandas as pd code_df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13', header=0)[0] # 종목코드가 6자리이기 때문에 6자리를 맞춰주기 위해 설정해줌 code_df.종목코드 = code_df.종목코드.map('{:06d}'.format) # 우리가 필요한 것은 회사명과 종목코드이기 때문에 필요없는 column들은 제외해준다. code_df = code_df[['회사명', '종목코드']] # 한글로된 컬럼명을 영어로 바꿔준다. code_df = code_df.rename(columns={'회사명': 'name', '종목코드': 'code'}) code_df.head()
 
-출처: https://excelsior-cjh.tistory.com/109 [EXCELSIOR]
+# run main
+if __name__ == '__main__':
+    # arr = [1, 2, 3, 4, 5];
+    # print(arr[::-5])
+    # print(arr[1:2])
+    # a = len([1,2,3,3])//3
+    l = [1, 2, 3, 3]
+    l.__len__()
+    a = len(l) / 3
+    print(a)
